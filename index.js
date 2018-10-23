415e8efa74f251d98622350057e5f6c234807d56
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-23 01:12:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 /* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'george'
+  leastFavoriteCustomer = 'joe'
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'mo'
+  let favoriteCustomer = 'curly'
+}
