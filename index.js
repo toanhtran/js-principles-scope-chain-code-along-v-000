b7e4709e47a7aac08aa937358ecc4081e120422a
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-23 01:08:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function overWriteBestCustomer() {
 }
 
 function reassignLeastFavorite() {
-  LeastFavoriteCustomer = 'joe'
+  leastFavoriteCustomer = 'joe'
   changeLeastFavoriteCustomer = 'george'
 }
 
